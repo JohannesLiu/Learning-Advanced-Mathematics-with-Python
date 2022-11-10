3038fc694fa83e09c2a0be7f46697922234056b7
johannesliu <johannesliu@live.com>
2022-11-10 00:58:55 -0600

    update

--- deploy.py
+++ deploy.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Johannes Liu
+LastEditors: Johannes Liu
+email: iexkliu@gmail.com
+github: https://github.com/johannesliu
+Date: 2022-09-26 08:39:11
+LastEditTime: 2022-11-10 00:49:53
+motto: Still water run deep
+Description: Modify here please
+FilePath: \Learning_Advanced_Mathematics_with_Python\deploy.py
+'''
 import os
 
-# os.system("python ./compile.py")
+os.system("gitbook build")
 os.system("git add -A")
 os.system("git commit -m \"update\"")
 os.system("git push")
